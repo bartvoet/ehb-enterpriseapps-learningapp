8925a65b38149b3180f77593a8dabfb9c2cabd51
Bart Voet <bart_voet@telenet.be>
2022-11-27 15:26:37 +0100

    Initial command for EBH-project

---
+++ src/main/java/be/ehb/bvo/leanring/EhbEnterpriseappsLearningappApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.bvo.leanring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhbEnterpriseappsLearningappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhbEnterpriseappsLearningappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/bvo/leanring/EhbEnterpriseappsLearningappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.bvo.leanring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhbEnterpriseappsLearningappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
