9a3c7ad7c052f2cae8d692d3bceff66f9c95551e
Bart Voet <bart_voet@telenet.be>
2023-01-09 21:14:31 +0100

    Allowing js and css to be loaded without authentication

--- src/main/java/be/ehb/bvo/leanring/WebSecurityConfig.java
+++ src/main/java/be/ehb/bvo/leanring/WebSecurityConfig.java
@@ -18,7 +18,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/ui/user/create").permitAll()
+                        .requestMatchers("/", "/ui/user/create", "/resources/**", "/static/**","/webjars/**")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
